d039625195e9cc315473810166f824a3de3e5a7a
Bikram Basnett <Bikrambasnett7@gmal.com>
2025-01-08 15:20:53 +0530

    Some update

--- musicPlayer/src/components/UI/SongCardBtn.jsx
+++ musicPlayer/src/components/UI/SongCardBtn.jsx
@@ -2,6 +2,7 @@ import { React, useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
+
 export default function CardBtn({
   songData,
   currentlyPlaying,
@@ -21,10 +22,12 @@ export default function CardBtn({
       if (isPlaying) {
         await audioRef.current.pause();
         setIsplaying(false);
+   
       } else {
         await audioRef.current.play();
         setIsplaying(true);
         setCurrentlyPlaying(audioRef.current);
+       
       }
 
       setIsplaying(!isPlaying);
@@ -36,11 +39,11 @@ export default function CardBtn({
   return (
     <button
       onClick={playSong}
-      className="h-10 w-10 rounded-full border-[1px] border-[#9c227c] bg-white/30 backdrop-blur-[3px] flex justify-center items-center cursor-pointer "
+      className="h-9 w-9 rounded-full border-[1px] border-[#9c227c] bg-white/30 backdrop-blur-[3px] flex justify-center items-center cursor-pointer "
     >
       <FontAwesomeIcon
         icon={isPlaying ? faPause : faPlay}
-        className="text-white/50 text-[18px] hover:text-white"
+        className="text-white/50 text-[16px] hover:text-white"
       />
     </button>
   );
