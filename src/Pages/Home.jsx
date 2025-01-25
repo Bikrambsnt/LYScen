531f6fb291262132b5ca6aad12dc2f70d1ea896f
Bikram Basnett <Bikrambasnett7@gmail.com>
2025-01-25 20:09:17 +0530

    Homepage

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Song,Artist,Trending} from '../components/index'
+
+function Home() {
+
+
+
+
+    return(
+        <div>
+
+           <Song/>
+           <Trending/>
+           <Artist/>
+
+        </div>
+    )
+}
+export default Home;
