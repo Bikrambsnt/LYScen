9103d4b78764d33589f4eb55178825fb73fdaab7
Bikram Basnett <Bikrambasnett7@gmail.com>
2025-01-26 18:20:29 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { Header,Home} from "./components/index";
+import { Header, Home } from "./components/index";
 import { React, useState, useEffect } from "react";
 import { useAudioProvider } from "./hook/useAudioProvider";
 import SongProgressBar from "./components/UI/playingProgressBar/SongProgressBar";
-import { BrowserRouter as Router, Routes, Route,useLocation } from "react-router-dom";
-import {SearchBar} from "./components/index";
-
-
-
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { SearchBar } from "./components/index";
 
 function App() {
   // Toggle Bg theme
@@ -33,33 +34,32 @@ function App() {
     }
   }, [darkMode]);
 
-
   return (
     <Router>
-      <Content darkMode={darkMode} setDarkMode={setDarkMode}/>
-      </Router>
+      <Content darkMode={darkMode} setDarkMode={setDarkMode} />
+    </Router>
   );
-  }
-   
-  //To wrap every components inisde a Router.
-   function Content ({darkMode,setDarkMode}) {
+}
 
-    const location =useLocation();
+//To wrap every components inisde a Router.
+function Content({ darkMode, setDarkMode }) {
+  const location = useLocation();
 
-    return(
-     <div
-       className={` w-screen h-max transition-colors duration-200 ease-in ${
-         darkMode ? "bg-[#080808] text-white" : "bg-white text-black"
-       }`}
-     >
-       {location.pathname !=="/search" && <Header darkMode={darkMode} setDarkMode={setDarkMode} />}
-       <Routes>
-       <Route path="/"  element={<Home />} />
-       <Route path="/search" element={<SearchBar />} />
-       </Routes>
-     </div>
-     );
-   }
-
+  return (
+    <div
+      className={` w-screen h-max transition-colors duration-200 ease-in ${
+        darkMode ? "bg-[#080808] text-white" : "bg-white text-black"
+      }`}
+    >
+      {location.pathname !== "/search" && (
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      )}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchBar />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
