75ac1d5c0978a347ba404a9978d7d4c1bb8e6108
Bikrambsnt <Bikrambasnett7@gmail.com>
2025-03-16 18:01:23 +0530

    Remove file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,11 @@
 import { React, useState } from "react";
 import { Song, Artist, Trending } from "../components/index";
-import Album from "../components/UI/Album";
+
 
 function Home() {
   return (
     <div className="w-screen p-2">
       <Song />
-      <Album/>
       <Trending />
       {/* <Artist /> */}
     </div>
